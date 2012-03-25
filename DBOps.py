548678d3f5603a9d02dd9ba2398bf1efa778ced7
Ben <sheepskin505@gmail.com>
2012-03-25 13:50:39 -0400

    Refactored out the db specific commands except for views. Views are really tricky to create generically.

--- DBOps.py
+++ DBOps.py
@@ -30,4 +30,159 @@ class DBOps():
             return []
         conn.close()
         
+    def createTable(self, tableName, pk_head, unique_head, other_head):
+        """
+        pk_head should be the form pk_name and is the primary key 
+        column. If None, an id field is automatically created. The type should either be
+        'serial' or 'integer'. DO NOT PK OFF OF A STRING.
+        
+        unique_head and other_head is of the form: (col name, type). Type can be any
+        valid db type. 
+        
+        unique_head is a list of unique key values. Note that id can be assumed to exist,
+        so long as pk_head contains id or is left blank.
+        """
+        if pk_head == None: pk_head = []
+        if unique_head == None: unique_head = []
+        if other_head == None: other_head = []
+        #print tableName, pk_head, unique_head, other_head
+        logging.info("Trying to drop table %s" % tableName)
+        if self.execute("DROP TABLE %s CASCADE;" % tableName) == None:
+            logging.error("Dropping table %s failed" % tableName)
+        else:
+            logging.info("dropped table %s" % tableName)
+        
+        cmd_str = "CREATE TABLE %s (" % tableName
+        if len(pk_head) != 0:
+            cmd_str = "%s %s %s primary key, " % (cmd_str, pk_head[0], pk_head[1])
+        else:
+            cmd_str = "%s id serial primary key, " % cmd_str
+        
+        for h, t in unique_head:
+            cmd_str = "%s %s %s, " % (cmd_str, h, t)
+        
+        for h, t in other_head:
+            cmd_str = "%s %s %s, " % (cmd_str, h, t)
+        
+        if len(unique_head) != 0:
+            cmd_str = "%s UNIQUE( " % cmd_str
+            for h, t in unique_head:
+                cmd_str = "%s %s, " % (cmd_str, h)
+            cmd_str = cmd_str.rstrip(",")
+            cmd_str = "%s ) " % cmd_str
+        cmd_str = cmd_str.rstrip(" ,") + (");")
+        logging.info("Trying to create table: %s" % tableName)
+        if self.execute(cmd_str) == None:
+            logging.error("Failed to create table: %s" % tableName)
+        else: 
+            logging.info("Created table: %s" % tableName)
+    
+    def insert(self, table, col_names=[], data=[] ):
+        """
+        insert will run: insert into table (col_names) values data
+        All of the data is wrapped in quotes and escaped. Postgeres is nice in that
+        it allows numbers to be wrapped in quotes as still enter as a number if the column name.
+        data needs to be in a matrix format with one entry for each column provided to col_names.
+        """
+        cmd_str = "INSERT INTO %s (" % table
+        for col in col_names:
+            cmd_str = "%s%s, " % (cmd_str, col)
+        cmd_str = cmd_str.rstrip(", ") + ") VALUES "
+        
+        for row in data:
+            cmd_str = "%s (" % cmd_str
+            for col in row:
+                cmd_str = "%s%s, " % (cmd_str, self.clean(col))
+            cmd_str = cmd_str.rstrip(", ") + "),\n"
+        cmd_str =  cmd_str.rstrip(",\n") + ";" 
+        
+        logging.debug("Running insert cmd")
+        if self.execute(cmd_str) == None:
+            logging.error("Insert command failed")
+        logging.debug("Insert cmd succeeded")
+    
+    def colTypeFromArray(self, line=[], headerType={}):
+        """
+        This will read each array object and try to determine the type.
+        headerTypes is used to compare the current guess for the cell. Initially
+        pass in an empty dictionary and on subsequent passes pass back the returned dictionary. 
+        If the length of the array object is zero, the type can not be determined
+        so "null" will be returned.
+        If the guess is currently float, but float fails to be parsed, convert to null 
+        (this should rarely if ever happen)
+        If guess is currently int but float passes, make the column a float
+        If int fails, make varchar(len(string))
+        
+        NOTE: for now only pass in non-zero length empty strings if working with
+        column delineated data. This will not work with data if the first iteration
+        the cell is one length and subsequent iterations have the cell a different length 
+        """
+        x = 0
+        for cell in line:
+            #print "X: %d ; Cell: %s ; Line[x]: %s" % (x, cell, line[x])
+            if x in headerType.keys():
+                #if x == 45 or x == 44:
+                #    print x, cell, line[x], headerType[x]
+                
+                if len(cell) == 0:
+                    x = x + 1
+                    #no information given about the data type, should assume string
+                    continue
+                else:
+                    ret = None
+                    try:
+                        #if passes an int test and the field is currently null make the field an int
+                        ret = int(cell)
+                        if headerType[x] == "null":
+                            headerType[x] = "integer"
+                        #if it's an integer don't change it, if it's a float don't change it
+                    except ValueError:
+                        ret = None
+                    
+                    if ret == None:
+                        try:
+                            #if passes float but the curent deffinition is not a string (int or null only)
+                            # change the type to a float
+                            ret = float(cell)
+                            if headerType[x] == "integer" or headerType[x] == "null":
+                                headerType[x] = "float" 
+                        except ValueError:
+                            ret = None
+                    if ret == None:
+                        if headerType[x] == "integer" or headerType[x] == "float" or headerType[x] == "null": 
+                            if len(cell.strip()) != 0:
+                                headerType[x] = "varchar(%s)" % len(cell)
+                        elif headerType[x].find("varchar") != -1:
+                            cur_length = headerType[x].strip(")").split("varchar(")[1]
+                            cur_length = int(cur_length)
+                            if len(cell) > cur_length:
+                                #print "Before: %s" % headerType[x]
+                                headerType[x] = "varchar(%s)" % len(cell)
+                                #print "After %s" % headerType[x]
+            else:
+                if len(cell) == 0:
+                    headerType[x] = "null"
+                else:
+                    ret = None
+                    try:
+                        ret = int(cell)
+                        headerType[x] = "integer"
+                    except ValueError:
+                        ret = None
+                    
+                    if ret == None:
+                        try:
+                            ret = float(cell)
+                            headerType[x] = "float"
+                        except ValueError:
+                            ret = None
+                    
+                    if ret == None:
+                        if len(cell.strip()) == 0:
+                            headerType[x] = "null" #empty doesn't really give you a type, hope that it will eventually
+                        else:
+                            headerType[x] = "varchar(%s)" % len(cell)
+            x = x + 1
+        return headerType
+    
         
--- ThreadedFiles.py
+++ ThreadedFiles.py
@@ -86,52 +86,7 @@ class ThreadFiles(threading.Thread):
         self.batchRows = batchRows
         self.col_re = re.compile(r"(?P<table_name>\w+)_(?P<col_name>\d+)")
             
-    def createTable(self, tableName, pk_head, unique_head, other_head):
-        """
-        pk_head should be the form pk_name and is the primary key 
-        column. If None, an id field is automatically created. The type should either be
-        'serial' or 'integer'. DO NOT PK OFF OF A STRING.
-        
-        unique_head and other_head is of the form: (col name, type). Type can be any
-        valid db type. 
-        
-        unique_head is a list of unique key values. Note that id can be assumed to exist,
-        so long as pk_head contains id or is left blank.
-        """
-        if pk_head == None: pk_head = []
-        if unique_head == None: unique_head = []
-        if other_head == None: other_head = []
-        #print tableName, pk_head, unique_head, other_head
-        logging.info("Trying to drop table %s" % tableName)
-        if self.execute("DROP TABLE %s CASCADE;" % tableName) == None:
-            logging.error("Dropping table %s failed" % tableName)
-        else:
-            logging.info("dropped table %s" % tableName)
-        
-        cmd_str = "CREATE TABLE %s (" % tableName
-        if len(pk_head) != 0:
-            cmd_str = "%s %s %s primary key, " % (cmd_str, pk_head[0], pk_head[1])
-        else:
-            cmd_str = "%s id serial primary key, " % cmd_str
-        
-        for h, t in unique_head:
-            cmd_str = "%s %s %s, " % (cmd_str, h, t)
-        
-        for h, t in other_head:
-            cmd_str = "%s %s %s, " % (cmd_str, h, t)
-        
-        if len(unique_head) != 0:
-            cmd_str = "%s UNIQUE( " % cmd_str
-            for h, t in unique_head:
-                cmd_str = "%s %s, " % (cmd_str, h)
-            cmd_str = cmd_str.rstrip(",")
-            cmd_str = "%s ) " % cmd_str
-        cmd_str = cmd_str.rstrip(" ,") + (");")
-        logging.info("Trying to create table: %s" % tableName)
-        if self.execute(cmd_str) == None:
-            logging.error("Failed to create table: %s" % tableName)
-        else: 
-            logging.info("Created table: %s" % tableName)
+    
     
     def clean(self, text):
         """
@@ -157,29 +112,7 @@ class ThreadFiles(threading.Thread):
         except ValueError:
             return(text)
     
-    def insert(self, table, col_names=[], data=[] ):
-        """
-        insert will run: insert into table (col_names) values data
-        All of the data is wrapped in quotes and escaped. Postgeres is nice in that
-        it allows numbers to be wrapped in quotes as still enter as a number if the column name.
-        data needs to be in a matrix format with one entry for each column provided to col_names.
-        """
-        cmd_str = "INSERT INTO %s (" % table
-        for col in col_names:
-            cmd_str = "%s%s, " % (cmd_str, col)
-        cmd_str = cmd_str.rstrip(", ") + ") VALUES "
-        
-        for row in data:
-            cmd_str = "%s (" % cmd_str
-            for col in row:
-                cmd_str = "%s%s, " % (cmd_str, self.clean(col))
-            cmd_str = cmd_str.rstrip(", ") + "),\n"
-        cmd_str =  cmd_str.rstrip(",\n") + ";" 
-        
-        logging.debug("Running insert cmd")
-        if self.execute(cmd_str) == None:
-            logging.error("Insert command failed")
-        logging.debug("Insert cmd succeeded")
+    
     
     def createMetaTables(self, dict_cols):
         """
@@ -197,91 +130,6 @@ class ThreadFiles(threading.Thread):
             for k in sorted(dict_cols[key].keys()):
                 data.append([k, dict_cols[key][k][0]])
             self.insert(table, ["header", "description"], data)
-    
-    def colTypeFromArray(self, line=[], headerType={}):
-        """
-        This will read each array object and try to determine the type.
-        headerTypes is used to compare the current guess for the cell. Initially
-        pass in an empty dictionary and on subsequent passes pass back the returned dictionary. 
-        If the length of the array object is zero, the type can not be determined
-        so "null" will be returned.
-        If the guess is currently float, but float fails to be parsed, convert to null 
-        (this should rarely if ever happen)
-        If guess is currently int but float passes, make the column a float
-        If int fails, make varchar(len(string))
-        
-        NOTE: for now only pass in non-zero length empty strings if working with
-        column delineated data. This will not work with data if the first iteration
-        the cell is one length and subsequent iterations have the cell a different length 
-        """
-        x = 0
-        for cell in line:
-            #print "X: %d ; Cell: %s ; Line[x]: %s" % (x, cell, line[x])
-            if x in headerType.keys():
-                #if x == 45 or x == 44:
-                #    print x, cell, line[x], headerType[x]
-                
-                if len(cell) == 0:
-                    x = x + 1
-                    #no information given about the data type, should assume string
-                    continue
-                else:
-                    ret = None
-                    try:
-                        #if passes an int test and the field is currently null make the field an int
-                        ret = int(cell)
-                        if headerType[x] == "null":
-                            headerType[x] = "integer"
-                        #if it's an integer don't change it, if it's a float don't change it
-                    except ValueError:
-                        ret = None
-                    
-                    if ret == None:
-                        try:
-                            #if passes float but the curent deffinition is not a string (int or null only)
-                            # change the type to a float
-                            ret = float(cell)
-                            if headerType[x] == "integer" or headerType[x] == "null":
-                                headerType[x] = "float" 
-                        except ValueError:
-                            ret = None
-                    if ret == None:
-                        if headerType[x] == "integer" or headerType[x] == "float" or headerType[x] == "null": 
-                            if len(cell.strip()) != 0:
-                                headerType[x] = "varchar(%s)" % len(cell)
-                        elif headerType[x].find("varchar") != -1:
-                            cur_length = headerType[x].strip(")").split("varchar(")[1]
-                            cur_length = int(cur_length)
-                            if len(cell) > cur_length:
-                                #print "Before: %s" % headerType[x]
-                                headerType[x] = "varchar(%s)" % len(cell)
-                                #print "After %s" % headerType[x]
-            else:
-                if len(cell) == 0:
-                    headerType[x] = "null"
-                else:
-                    ret = None
-                    try:
-                        ret = int(cell)
-                        headerType[x] = "integer"
-                    except ValueError:
-                        ret = None
-                    
-                    if ret == None:
-                        try:
-                            ret = float(cell)
-                            headerType[x] = "float"
-                        except ValueError:
-                            ret = None
-                    
-                    if ret == None:
-                        if len(cell.strip()) == 0:
-                            headerType[x] = "null" #empty doesn't really give you a type, hope that it will eventually
-                        else:
-                            headerType[x] = "varchar(%s)" % len(cell)
-            x = x + 1
-        return headerType
-    
     
     def columnTypes(self, dict_cols, files):
         """
@@ -307,7 +155,7 @@ class ThreadFiles(threading.Thread):
             will change the datatype if the need arises. 
             
             """
-            headerType = self.colTypeFromArray(line, headerType)
+            headerType = self.myDBOpts.colTypeFromArray(line, headerType)
         return headerType
                 
     
@@ -336,7 +184,7 @@ class ThreadFiles(threading.Thread):
                     colType = "varchar(255)"
                 headers.append((self.colName(col), colType))
                 
-            self.createTable( table, ok_head, None, headers )
+            self.myDBOpts.createTable( table, ok_head, None, headers )
             
             #create the m table
             table = "%s_m" % key
@@ -346,7 +194,7 @@ class ThreadFiles(threading.Thread):
                 if colType == "null":
                     colType = "varchar(255)"
                 headers.append((self.colName(col), colType))
-            self.createTable( table, ok_head, None, headers )
+            self.myDBOpts.createTable( table, ok_head, None, headers )
     
     def parseGeoLine(self, dict_lookup, line, strip=True):
         """This will parase a single line of text using the dict_lookup.
@@ -416,11 +264,11 @@ class ThreadFiles(threading.Thread):
                         other_head.append((headers[header_idx], "varchar(255)"))
                     else:
                         other_head.append((headers[header_idx], headerType[header_idx]))
-            self.createTable(tableName, pk_head, None, other_head)
+            self.myDBOpts.createTable(tableName, pk_head, None, other_head)
     
     def batchGeoInsert(self, table, col_names, data):
         """
-        A seperate geo insert function is needed since
+        A separate geo insert function is needed since
         this data is much less complicated than the e* and m* data.
         """ 
         logging.debug("Pushing data")
@@ -463,7 +311,7 @@ class ThreadFiles(threading.Thread):
             for col_name in sorted(dict_cols[tableName].keys()):
                 col_names.append(self.colName(col_name))
             logging.info("Inserting data to table: %s_%s" % (tableName, table_suffix))
-            self.insert("%s_%s" % (tableName, table_suffix), col_names, data[tableName] )
+            self.myDBOpts.insert("%s_%s" % (tableName, table_suffix), col_names, data[tableName] )
         logging.debug("Finishing pushing data")
             
     def insertDataFromFile(self, file_handle, table_suffix, dict_cols, pk_index):
@@ -521,6 +369,8 @@ class ThreadFiles(threading.Thread):
             e_file.close()
             m_file.close()
     
+    #TODO: Figure out a good way to put this into DBOps. Creating views is very
+    #easy but highly customizable. Think about a good way to go about doing that. 
     def createViews(self, dict_cols, singleFile):
         """
         Creates the default views for all of the e and m tables.
@@ -537,7 +387,7 @@ class ThreadFiles(threading.Thread):
                 cmd_str = "%se.%s as %s, m.%s as %s_error, \n" % (cmd_str, col_name, col_name, col_name, col_name)
             cmd_str = cmd_str.strip(", \n")
             cmd_str = "%s \nfrom %s_e e \njoin %s_m m on e.LOGRECNO = m.LOGRECNO;" % (cmd_str, table_name, table_name)
-            self.execute(cmd_str)
+            self.myDBOpts.execute(cmd_str)
     
     def seqInsert(self, singleFile):
         """
